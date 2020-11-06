63e7929d418b10317826717c6606caaab57fe995
Brodie Giesbrecht <brodiejgiesbrecht@gmail.com>
2020-11-06 11:25:50 -0600

    Changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resource :customers, only: %i[show index]
-  get "/alpha", to: "customers#alpha_sort"
-  get "/missing_email", to: "customers#missing_email"
+  get "/customers/alphabetized", to: "customers#alpha_sort"
+  get "/customers/missing_email", to: "customers#missing_email"
   root to: "customers#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
